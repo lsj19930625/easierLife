861a77c5b25c8ecfceb0418394bffa035449764c
lisujian <813595706@qq.com>
2016-08-12 21:47:26 +0800

    前台和后台http交互

---
+++ main.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const port = process.env.PORT || 3000;
+const path = require('path');
+const app = express();
+// app.set("views","../easierLife/app/");
+// app.set('view engine','html');
+//const router = express.Router();
+app.use(express.static(path.join(__dirname, 'app')));
+
+app.get('/', (req, res) => {
+  // r.table("projects").orderBy('createdAt').run().then(result => {
+  //   res.send(result)
+  // }).catch(err => {
+  //   console.log("Error:", err)
+  // })
+  
+  // res.render("app/index.html",{
+
+  // })
+  //res.sendFile('localhost:3000/easierLife/app/index.html');
+  res.sendFile(__dirname+"/app/index.html");
+  //res.send('Got a get request');
+})
+
+app.post("/login",function(req, res){
+  console.log(req);
+  //res.json({msg:"got it"});
+  //res.send(req);
+});
+// app.post('/', (req, res) => {
+//   // const project = {
+//   //   'title': req.body.title,
+//   //   'content': req.body.content,
+//   //   'createdAt': new Date().toJSON()
+//   // }
+//   // r.table('projects').insert(project).run().then(result => {
+//   //   res.send(result)
+//   // }).catch(err => {
+//   //   console.log('Error:', err)
+//   // })
+//   res.send('Got a POST request');
+//   app.render("app/index.html",{
+
+//   })
+//   console.log("post index");
+// })
+
+// router.get('/login', (req, res) => {
+// 	res.render("app/index.html",{
+
+// 	})
+// 	console.log("get login");
+// })
+
+// router.post('/login', jsonParser, (req, res) => {
+//   console.log("post login");
+// })
+
+app.listen(port,"localhost",function(err){
+	if(err){
+		console.log(err);
+		return;
+	};
+	console.log('Listening at http://localhost:'+port)
+});
+
